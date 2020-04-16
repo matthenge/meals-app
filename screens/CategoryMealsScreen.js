e874f89b7e50a9c9c551ed94e1b8046eb813bf8e
James Maruhi <james.maruhi@tracom.co.ke>
2020-04-16 23:47:26 +0300

    Adding meals

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Platform } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Platform } from 'react-native';
 
-import { CATEGORIES } from '../data/dummy-data';
-import Colors from '../constants/Colors'
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = props => {
+    const renderMealItem = itemData => {
+        return (
+            <MealItem 
+                title={itemData.item.title}
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {
+                    props.navigation.navigate({
+                        routeName: 'MealDetail',
+                        params: {
+                            mealId: itemData.item.id
+                        }
+                    })
+                }}
+            />
+        );
+    };
+
     const catId = props.navigation.getParam('categoryId');
 
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+    const displayedMeals = MEALS.filter(
+        meal => meal.categoryIds.indexOf(catId) >= 0
+    );
 
     return (
         <View style={styles.screen}>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Meal Details!" onPress={() => {
-                props.navigation.navigate({routeName: 'MealDetail'});
-            }}/>
-            <Button title="Go Back!" onPress={() => {
-                props.navigation.goBack();
-            }}/>
+            <FlatList 
+                data={displayedMeals}
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+                style={{width: '100%'}}
+            /> 
         </View>
     );
 };
